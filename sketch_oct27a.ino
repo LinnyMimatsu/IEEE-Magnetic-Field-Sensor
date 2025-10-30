a986aff1db6c94096238adeed64c8d9b46620dd4
Dominic <dapressler@gmail.com>
2025-10-30 14:49:06 -0500

    fixing parts of the code

--- sketch_oct27a.ino
+++ sketch_oct27a.ino
@@ -2,16 +2,20 @@
 
 Servo myservo;
 
-const int hallPin = A0;      // Analog pin connected to VOUT of AD22151
+const int hallPin = A0; // Analog pin connected to VOUT of AD22151
 const int servoPin = 2; //Positive pin for motor
-float refVoltage = 5.00;    
+
+float refVoltage = 5.00;  //base voltage
 float voltage = 0;
+
+//Values for sensor and servo motor
 int sensorValue = 0;
 int servoPos = 0;
 
 // Thresholds for activation
 float lowThreshold = 1.;
 float highThreshold = 2.150;
+
 
 void setup() {
   Serial.begin(9600);
@@ -20,7 +24,10 @@ void setup() {
   delay(500);
 }
 
+
+
 void loop() {
+  
   // Read sensor
   sensorValue = analogRead(hallPin);
   voltage = sensorValue * (refVoltage / 1023.0);
@@ -33,7 +40,7 @@ void loop() {
   Serial.print(voltage, 3);
   Serial.print(" V");
 
-  // Detect polarity (relative to neutral)
+  // Detect polarity (relative to neutral) and print to the terminal
   if (voltage > 1.750)
     Serial.print("  → South Pole (Positive Field)");
   else if (voltage < 2.150)
